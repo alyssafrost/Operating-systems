8450410fda7583df4c13ca2243944623cffe9558
Alyssa Frost <frost7@uab.edu>
2021-08-03 14:46:17 -0500

    More notes

--- hw4/hw4.c
+++ hw4/hw4.c
@@ -20,9 +20,11 @@ To implement a simple job scheduler that executes non-interactive jobs
 (e.g., jobs that do not have direct user interaction,jobs that can run in the background).
 */
 
-// TODO: Create functionality for a "job" -- or an executable being run
-
-// TODO: Create functionality for an optional terminal input being provided from the user
+// TODO: Create functionality for an optional terminal input being provided from the user -- flags are assumed to be correct
 
     // int main(int argc, char **argv)
    
+// TODO: Implement a queue that we can add our jobs to, to be threaded to our "execution"
+// TODO: Implement a "submit" job -- or an executable being run, such as "hw3.c" is an executable"
+// TODO: Implement a "show jobs" option that allows you to print and list out all jobs both running, and waiting 
+
