e2328c04b8b958c505666cedeb4fd8090c6f24d7
Alyssa Frost <frost7@uab.edu>
2021-08-03 13:38:13 -0500

    Add identifier comments

---
+++ hw4/hw4.c
@@ -0,0 +1,21 @@
+/*
+Name(s): 
+- Alyssa Frost
+- Mallory Merkel
+---
+BlazerId(s): 
+- frost7
+- mmerkel
+---
+Project #: 4
+---
+To compile: 
+To run: 
+*/
+
+
+/*
+Objective:
+To implement a simple job scheduler that executes non-interactive jobs 
+(e.g., jobs that do not have direct user interaction,jobs that can run in the background).
+*/
