697c361dbfb32d2b5c6309b05b42f9f0edecf2fa
Alyssa Frost <frost7@uab.edu>
2021-08-03 14:06:22 -0500

    Add TODOs

--- hw4/hw4.c
+++ hw4/hw4.c
@@ -19,3 +19,10 @@ Objective:
 To implement a simple job scheduler that executes non-interactive jobs 
 (e.g., jobs that do not have direct user interaction,jobs that can run in the background).
 */
+
+// TODO: Create functionality for a "job" -- or an executable being run
+
+// TODO: Create functionality for an optional terminal input being provided from the user
+
+    // int main(int argc, char **argv)
+   
